37b0f60b8d276af9371173257e65b9d21a4b9e20
FauziyyahHendri <zizi.hendri2009@gmail.com>
2025-04-30 14:12:40 +0700

    Tugas PPLG

---
+++ Tugas7.php
@@ -0,0 +1,12 @@
+
+<?php
+$akifahsehat = true;
+$waliakifahsehat = true;
+
+if ($akifahsehat && $waliakifahsehat)  {
+    echo "Maka akifah berangkat sekolah";
+} else {
+    echo "Maka akifah tidak berangkat sekolah";
+}
+
+
