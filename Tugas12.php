9499a580a8a1885af18ef1f1262b6f8076421a71
FauziyyahHendri <zizi.hendri2009@gmail.com>
2025-05-15 14:48:34 +0700

    Tugas baru lagi

---
+++ Tugas12.php
@@ -0,0 +1,24 @@
+
+<?php
+
+$DaftarAnggota10B = [
+    "Akifah Zahrani Arafat",
+    "Alqumaerah",
+    "Althaf Azzah Zahra",
+    "Andi Alfirqahtunnajiyah Ahsyar",
+    "Andi Mufrodatul Fanzila",
+    "Fahrah Furaidah Sahrir",
+    "Fauziyyah Princess",
+    "Jilan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabiatul Adawiyah",
+    "Alifah Izzah Rahmadini"
+
+];
+$DaftarAnggota10B []= "Muftihaturrahmah";
+$DaftarAnggota10B []= "Auliyah Nastity Khayla Armada";
+$DaftarAnggota10B []= "Putri Kinanti Almagfirah";
+echo "Daftar Anggota Kelas 10 B";
+foreach ($DaftarAnggota10B as $Anggotakelas10) {
+    echo "-" . $Anggotakelas10 . "</br>";
+}
